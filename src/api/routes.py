a410b1b677eb5847cc7aeafbe2c2ac4ae3d21281
Rivero Peña Jose Pedro <129573462+PedroRiverop@users.noreply.github.com>
2025-04-27 09:43:16 +0000

    Intento 10.10 para deplaoy sin src y frontend da 404

--- src/api/routes.py
+++ src/api/routes.py
@@ -2,7 +2,7 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from src.pi.models import db, Lead
+from src.api.models import db, Lead
 from src.api.utils import generate_sitemap, APIException
 from flask_cors import CORS, cross_origin
 
